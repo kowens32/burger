6e3918ef9b4286eae1bb23de7e38d2254d7fd7f5
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-17 21:06:25 -0500

    trying to figure out the router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,11 @@
-var express = require('express');
+var express = require("express");
+
+var router = express.Router();
 var burger = require(../models/burger.js);
+
+//router for the app
+
+
+
+
+module.exports = router;
