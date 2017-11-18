37c10830e67148e3f2adbdc265d6ba0bdad2a51c
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-17 21:55:31 -0500

    updating burger js

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,23 @@
 var orm = require(../config/orm.js);
 
 var burgers = {
-    orm.selectAll()
-}
+   selectAll: function(){
+       orm.selectAll('burgers', function(res) {
+           cb(res)
+       });
+},
+
+    insertOne: function() {
+       orm.insertOne('burgers',col,value, function(res){
+           cb(res);
+       });
+    },
+
+    updateOne: function() {
+       orm.updateOne('burgers', col, value, function(res){
+           cb(res);
+       });
+    }
+};
+
+module.exports = burgers;
